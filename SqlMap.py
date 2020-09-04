dcbc5619b069602121316601e9d7db7eca13b30b
xRiseStrqfe <68116540+xRiseStrqfe@users.noreply.github.com>
2020-09-04 22:58:01 +0300

    Update SqlMap.py

--- SqlMap.py
+++ SqlMap.py
@@ -1,8 +1,9 @@
 import os
+from colorama import Fore, Back, Style
 
 os.system("clear")
 
-banner = """
+banner = print(Fore.BLUE+"""
   ██████   █████   ██▓        ███▄ ▄███▓ ▄▄▄       ██▓███  
 ▒██    ▒ ▒██▓  ██▒▓██▒       ▓██▒▀█▀ ██▒▒████▄    ▓██░  ██▒
 ░ ▓██▄   ▒██▒  ██░▒██░       ▓██    ▓██░▒██  ▀█▄  ▓██░ ██▓▒
@@ -14,15 +15,17 @@ banner = """
       ░      ░        ░  ░          ░         ░  ░         
                                                            
                         GitHub: https://github.com/xRiseStrqfe
-                        Discord: https://discord.gg/heCDv6V
-"""
-
+                        Discord: https://discord.gg/heCDv6V        
+               """)
 print(banner)
 
 url = input("Sql Açığı Aranan Site ----->")
+level = input("Taramayı Ne Kadar Ayrıntıyla Yapalım----->")
+risk = input("Risk Seviyesini Belirleyin----->")
+
 
 os.system("clear")
-os.system("sqlmap -u"+url+"--dbs")
+os.system("sqlmap -u "+url+"--dbs --level="+level+" --risk="+risk+" --batch -randomagent")
 
 print("Sql Injection Açıkğı Araması Bitmiştir, Bizi Tercih Ettiğiniz İçin Teşekkürler.")
 print("A xRiseStrqfe Script")
